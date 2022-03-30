c7e15020e66e44a2272b516d694fde4038d32a93
rodrigoplotze <rodrigoplotze@gmail.com>
2022-03-30 12:37:38 -0300

    app04

---
+++ app05_ibge/lib/view/tela_detalhes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TelaDetalhes extends StatefulWidget {
+  const TelaDetalhes({ Key? key }) : super(key: key);
+
+  @override
+  State<TelaDetalhes> createState() => _TelaDetalhesState();
+}
+
+class _TelaDetalhesState extends State<TelaDetalhes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ app05_ibge/lib/view/tela_principal.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({ Key? key }) : super(key: key);
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IBGE'),
+        backgroundColor: Colors.black87,
+      ),
+      backgroundColor: Colors.white24,
+      
+    );
+  }
+}
