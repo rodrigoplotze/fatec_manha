acbe0e4751c2e580eefd51d7d9a55439a338e171
Rodrigo Plotze <rodrigoplotze@gmail.com>
2024-03-07 08:46:28 -0300

    app04

--- app04/lib/main.dart
+++ app04/lib/main.dart
@@ -54,9 +54,26 @@ class _PrincipalViewState extends State<PrincipalView> {
           child: Column(
             children: [
               //
+              // Adicionar a imagem
+              //
+              Image.asset(
+                'lib/imagens/img1.jpg',
+                width: 200,
+                height: 200,
+              ),
+              Image.network(
+                'https://picsum.photos/200/200',
+                width: 100,
+                height: 100,
+              ),
+              SizedBox(height: 30),
+
+              //
               // CAMPO DE TEXTO
               //
               TextFormField(
+                controller: txtValor1,
+
                 style: TextStyle(fontSize: 32),
                 decoration: InputDecoration(
                   labelText: 'Valor 1',
@@ -66,27 +83,38 @@ class _PrincipalViewState extends State<PrincipalView> {
                 //
                 // VALIDAÇÃO
                 //
-                validator: (value){
-                  if (value ==null){
+                validator: (value) {
+                  if (value == null) {
                     return 'Informe um valor';
-                  }else if (value.isEmpty){
-                    return 'Informe um valor';  
-                  }else if (double.tryParse(value)==null){
-                    return 'Informe um valor NUMÉRICO'; 
+                  } else if (value.isEmpty) {
+                    return 'Informe um valor';
+                  } else if (double.tryParse(value) == null) {
+                    return 'Informe um valor NUMÉRICO';
                   }
                   //Retornar null significa sucesso na validação
                   return null;
                 },
-
               ),
               SizedBox(height: 30),
               TextFormField(
+                controller: txtValor2,
                 style: TextStyle(fontSize: 32),
                 decoration: InputDecoration(
                   labelText: 'Valor 2',
                   border: OutlineInputBorder(),
                   prefixIcon: Icon(Icons.heart_broken),
                 ),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Informe um valor';
+                  } else if (value.isEmpty) {
+                    return 'Informe um valor';
+                  } else if (double.tryParse(value) == null) {
+                    return 'Informe um valor NUMÉRICO';
+                  }
+                  //Retornar null significa sucesso na validação
+                  return null;
+                },
               ),
               SizedBox(height: 30),
               //
@@ -97,18 +125,33 @@ class _PrincipalViewState extends State<PrincipalView> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue.shade100,
                   foregroundColor: Colors.blue.shade900,
-                  minimumSize: Size(200,50),
+                  minimumSize: Size(200, 50),
                   shadowColor: Colors.red,
                 ),
                 onPressed: () {
-
                   //
-                  // Chamar os validadores dos campos de texto 
+                  // Chamar os validadores dos campos de texto
                   //
-                  if (formKey.currentState!.validate()){
+                  if (formKey.currentState!.validate()) {
                     //Form validado com sucesso
-                  }
 
+                    //
+                    // Retornar as informações inseridas
+                    // nos campos de texto
+                    //
+                    setState(() {
+                      double v1 = double.parse(txtValor1.text);
+                      double v2 = double.parse(txtValor2.text);
+                      double s = (v1 + v2);
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Soma: ${s.toStringAsFixed(2)}'),
+                          duration: Duration(seconds: 3),
+                        ),
+                      );
+                    });
+                  }
                 },
                 child: Text('OK'),
               ),
